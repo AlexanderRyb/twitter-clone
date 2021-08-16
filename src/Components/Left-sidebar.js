4b718795302487eb636ebbbe230558407d71441c
Alexander <Transoceantrain@gmail.com>
2021-08-16 18:24:31 +0300

    test commit

--- src/Components/Left-sidebar.js
+++ src/Components/Left-sidebar.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import './lsidebar.scss'
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {library} from "@fortawesome/fontawesome-svg-core"
+import {fas} from "@fortawesome/free-solid-svg-icons"
+
+library.add(fas);
 function LeftSidebar(){
+
     return(
 <div className="leftSidebar">
 <nav>
     <a>Twitter Icon</a>
-    <a>Home</a>
-    <a>Explore</a>
-    <a>Notifications</a>
-    <a>Messages</a>
-    <a>Bookmarks</a>
-    <a>Lists</a>
-    <a>Profile</a>
-    <a>More</a>
+    <a><FontAwesomeIcon icon={["fas", "home"]} />Home</a>
+    <a><FontAwesomeIcon icon={["fas", "hashtag"]} /> Explore</a>
+    <a> <FontAwesomeIcon icon={["fas", "fa-bell-o"]} />Notifications</a>
+    <a> Messages</a>
+    <a> <FontAwesomeIcon icon={["fas", "bookmark"]} />Bookmarks</a>
+    <a> <FontAwesomeIcon icon={["fas", "list"]} />Lists</a>
+    <a> <FontAwesomeIcon icon={["fas", "person"]} />Profile</a>
+    <a> <FontAwesomeIcon icon={["fas", "more"]} />More</a>
+    
 
 </nav>
 </div>
